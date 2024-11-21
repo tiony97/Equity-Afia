ed7e33ceef73b76e3328355b65b4ad9eac36d452
tiony <kipanthony97@gmail.com>
2024-11-21 10:19:29 +0300

    Mobile Resp Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,6 @@ $(document).ready(function () {
 $(document).ready(function () {
 	const swiper = new Swiper(".services-slider", {
 		loop: false,
-		slidesPerView: 4.5,
 		spaceBetween: 12,
 		autoplay: {
 			delay: 5000,
@@ -110,12 +109,52 @@ $(document).ready(function () {
 		// Responsive breakpoints
 		breakpoints: {
 			// when window width is >= 320px
-			400: {
-				slidesPerView: 1,
+			320: {
+				slidesPerView: 1.2,
 			},
 			// when window width is >= 768px
 			768: {
+				slidesPerView: 2.1,
+			},
+			// when window width is >= 480px
+			1024: {
+				slidesPerView: 4.5,
+			},
+		},
+		effect: "slide",
+	});
+});
+
+/* Payments Slider */
+$(document).ready(function () {
+	const swiper = new Swiper(".pay-swiper", {
+		loop: false,
+		spaceBetween: 12,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+
+		// If we need pagination
+		pagination: {
+			el: ".swiper-pagination",
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+
+		// Responsive breakpoints
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
 				slidesPerView: 1.5,
+			},
+			// when window width is >= 768px
+			768: {
+				slidesPerView: 2.1,
 			},
 			// when window width is >= 480px
 			1024: {
